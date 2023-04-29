26f5060d5da2bf447dc99257fa1c7ea6cc6549cd
Art_Line <serg20004@ukr.net>
2023-04-29 03:15:15 +0200

    lesson-02

---
+++ lesson-02/script.js
@@ -0,0 +1,19 @@
+// task 01
+let username = prompt('enter username');
+alert('Hi ' + username + ' How are you?')
+
+
+// task 02
+let firstNumber = +prompt('enter first number');
+let secondNumber = +prompt('enter second number');
+console.log(firstNumber + ' + ' + secondNumber + ' = ' + (firstNumber + secondNumber));
+console.log(firstNumber + ' - ' + secondNumber + ' = ' + (firstNumber - secondNumber));
+console.log(firstNumber + ' * ' + secondNumber + ' = ' + (firstNumber * secondNumber));
+console.log(firstNumber + ' / ' + secondNumber + ' = ' + (firstNumber / secondNumber));
+
+
+// task 03
+let number1 = +prompt('enter number 1');
+let number2 = +prompt('enter number 2');
+let number3 = +prompt('enter number 3');
+alert((number1 + number2 + number3) / 3);
