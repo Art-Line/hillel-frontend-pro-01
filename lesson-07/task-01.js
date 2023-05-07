5894bc9f142222051b27cecfbaf938fe520453ef
Art_Line <serg20004@ukr.net>
2023-05-07 14:47:47 +0200

    class-7. recursive

---
+++ lesson-07/task-01.js
@@ -0,0 +1,15 @@
+// Implement a recursive function that raises a number to a power.
+// The number to be raised to the membership power as the first argument in the function
+// excluded as the second argument in the function
+// pow(num, degree)
+
+
+function pow(num, degree) {
+    if (degree > 1) {
+        return num * pow(num,  degree - 1)
+    }
+    return num;
+}
+
+let result = pow(2, 4);
+console.log(result);
